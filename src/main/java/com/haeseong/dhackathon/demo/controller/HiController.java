8ceb1511b77a85be1ad02e528a80c921b63f7e17
Haeseong Jeon <>
2018-07-31 00:09:37 +0900

    Initial commit

---
+++ src/main/java/com/haeseong/dhackathon/demo/controller/HiController.java
@@ -0,0 +1,32 @@
+package com.haeseong.dhackathon.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    // 최초로 메시지를 전송
+    @PostMapping("/hi1")
+    public String getHi1() {
+        return "{\"responseType\":\"inChannel\",\"text\":\"HelloWorld!\"}";
+    }
+
+    // 메시지를 보낸 후 추가로 전송
+    @PostMapping("/hi2")
+    public String getHi2() {
+        return "{\"replaceOriginal\":false,\"responseType\":\"inChannel\",\"text\":\"HelloWorld!\"}";
+    }
+
+    // 기존에 보낸 메시지를 업데이트
+    @PostMapping("/hi3")
+    public String getHi3() {
+        return "{\"responseType\":\"inChannel\",\"replaceOriginal\":true,\"text\":\"HelloWorld!(Updated)\"}";
+    }
+
+    // 기존에 보낸 메시지를 삭제하고 메시지를 새로 전송
+    @PostMapping("/hi4")
+    public String getHi4() {
+        return "{\"responseType\":\"inChannel\",\"deleteOriginal\":true,\"text\":\"HelloWorld!(Updated)\"}";
+    }
+}
